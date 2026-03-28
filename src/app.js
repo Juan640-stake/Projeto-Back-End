f0cb99ba140b994a7c1b551cc649505d4e0c3c84
Juan Medeiros <medeirosjuan53@gmail.com>
2026-03-28 20:43:12 -0300

    fix: adicionar cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json({ limit: '10mb' }));
 
 const userRoutes = require('./routes/user.routes');
